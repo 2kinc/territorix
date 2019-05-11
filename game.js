4d816b5c0686ece85cc744280c9fc7158818eb63
Ken Zhou <mailofrealcev@gmail.com>
2019-05-11 15:12:59 -0400

    render!!!!!

--- game.js
+++ game.js
@@ -5,7 +5,8 @@ function Tile(name, color, description) {
 }
 
 var tiles = {
-    grass: new Tile('Grass', '#44ee44', 'A tidy grass tile.')
+    grass: new Tile('Grass', '#44ee44', 'A tidy grass tile.'),
+    water: new Tile('Water', '#4488ff', 'A water tile. Need a boat to cross.')
 }
 
 function getTileId(coords) {
@@ -20,21 +21,33 @@ function Renderer(el, ref) {
     var that = this;
     this.el = $(el);
     this.ref = ref;
-    this.ref.child('tiles').on("child_added", function (snap) {
-        var tile = snap.val();
-        var coords = snap.key.split(',');
-        coords = {
-            x: Number(coords[0]),
-            y: Number(coords[1])
-        };
-        var td = document.createElement('td');
-        var info = tiles[tile.type];
-        td.innerText = info.name;
-        td.style.background = info.color;
-        td.title = info.description;
-        td.id = getTileId(coords);
-        that.el.append(td);
-    });
+    this.generate = function (rows, cols, start_x, start_y) {
+        for (var y = (start_y || 0); y < rows + (start_y || 0); y++) {
+            var tr = document.createElement('tr');
+            that.el.append(tr);
+            for (var x = (start_x || 0); x < cols + (start_x || 0); x++) {
+                var td = document.createElement('td');
+                td.id = getTileId({ x: x, y: y });
+                td.className = 'game-tile';
+                tr.appendChild(td);
+            }
+        }
+    }
+    this.initialize = function () {
+        that.ref.child('tiles').on("child_added", function (snap) {
+            var tile = snap.val();
+            var coords = snap.key.split(',');
+            coords = {
+                x: Number(coords[0]),
+                y: Number(coords[1])
+            };
+            var td = document.getElementById(getTileId(coords));
+            var info = tiles[tile.type];
+            td.innerText = info.name;
+            td.style.background = info.color;
+            td.title = info.description;
+        });
+    };
 }
 
 var app = firebase;
@@ -42,3 +55,5 @@ var db = app.database();
 var dbRef = db.ref('territorix').child('main-server');
 
 var renderer = new Renderer('#game-tiles', dbRef);
+renderer.generate(10, 10);
+renderer.initialize();
